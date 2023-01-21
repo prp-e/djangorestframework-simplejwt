1f027b7ad1e582e985053efb3ca9f69543c7ceed
Muhammadreza Haghiri <haghiri75@gmail.com>
2023-01-21 13:13:37 +0330

    still wip

--- rest_framework_simplejwt/serializers.py
+++ rest_framework_simplejwt/serializers.py
@@ -48,19 +48,27 @@ class TokenObtainSerializer(serializers.Serializer):
 
         self.user = authenticate(**authenticate_kwargs)
 
-        # if not api_settings.USER_AUTHENTICATION_RULE(self.user):
-        #     raise exceptions.AuthenticationFailed(
-        #         self.error_messages["no_active_account"],
-        #         "no_active_account",
-        #     )
-
-        if self.user is None:
+        if not api_settings.USER_AUTHENTICATION_RULE(self.user) and self.user is not None:
+            raise exceptions.AuthenticationFailed(
+                self.error_messages["no_active_account"],
+                "no_active_account",
+            )
+        else:
             self.error_messages['no_active_account'] = _(
                 'Credentials did not match')
             raise exceptions.AuthenticationFailed(
                 self.error_messages['no_active_account'],
                 'no_active_account',
             )
+
+
+        # if self.user is None:
+        #     self.error_messages['no_active_account'] = _(
+        #         'Credentials did not match')
+        #     raise exceptions.AuthenticationFailed(
+        #         self.error_messages['no_active_account'],
+        #         'no_active_account',
+        #     )
 
         return {}
 
