4081d1b7fcddc2fb6e9822a8e42d151fef4ea7e1
Muhammadreza Haghiri <haghiri75@gmail.com>
2023-01-21 12:42:41 +0330

    just added a simple error, work is in progress obviously

--- rest_framework_simplejwt/serializers.py
+++ rest_framework_simplejwt/serializers.py
@@ -48,6 +48,14 @@ class TokenObtainSerializer(serializers.Serializer):
 
         self.user = authenticate(**authenticate_kwargs)
 
+        if self.user is None:
+            self.error_messages['no_active_account'] = _(
+                'Credentials did not match')
+            raise exceptions.AuthenticationFailed(
+                self.error_messages['no_active_account'],
+                'no_active_account',
+            )
+
         if not api_settings.USER_AUTHENTICATION_RULE(self.user):
             raise exceptions.AuthenticationFailed(
                 self.error_messages["no_active_account"],
